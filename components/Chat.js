4c154932aff105bf1b4e848bd9b0d371c59c83a7
amro-mansour <amro8000@gmail.com>
2022-07-31 15:10:10 +0200

    Adding a messages state to the Chat screen
    the messages sent by the user will be stored in the state, every message sent will be appended to the last one

--- components/Chat.js
+++ components/Chat.js
@@ -1,20 +1,79 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
   componentDidMount() {
     let name = this.props.route.params.name; // OR ...
     // let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  // This function renders messages in a bubble form, the bubble can also be given a color
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          },
+        }}
+      />
+    );
   }
 
   render() {
     let { backGroundColor } = this.props.route.params;
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backGroundColor }}>
-        <Text>Hello there!</Text>
+      <View style={{ flex: 1, backgroundColor: backGroundColor }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {/* This fixs keyboards displaying errors on android devices  */}
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
-    )
+    );
   }
 }
